cea7ee5c4d2b674df80b93bba9a52e3080adf066
Deathridden <46332875+Deathridden@users.noreply.github.com>
2024-07-16 18:11:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     menuItems.forEach(item => {
-        item.addEventListener('click', () => {
-            menuItems.forEach(link => link.classList.remove('active'));
-            item.classList.add('active');
-            if (item.parentElement.classList.contains('dropdown')) {
+        item.addEventListener('click', (e) => {
+            const parentDropdown = item.parentElement.classList.contains('dropdown');
+            if (parentDropdown) {
+                e.preventDefault();
                 item.parentElement.querySelector('.dropdown-content').classList.toggle('show');
             } else {
+                menuItems.forEach(link => link.classList.remove('active'));
+                item.classList.add('active');
                 navMenu.classList.remove('show');
                 document.querySelectorAll('.dropdown-content').forEach(content => content.classList.remove('show'));
             }
